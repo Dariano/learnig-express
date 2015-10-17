0cc72d48bc281d9df982ba9a82c14e1b50c9bc49
dariano <darianosoares@hotmail.com>
2015-10-17 02:50:48 -0300

    Adicionado a FaturaModel.

---
+++ models/faturaModel.js
@@ -0,0 +1,19 @@
+function FaturaModel() { };
+
+FaturaModel.prototype.find = function(query, callback) {
+	
+};
+
+FaturaModel.prototype.create = function(data, callback) {
+	
+};
+
+FaturaModel.prototype.update = function(_id, data, callback) {
+	
+};
+
+FaturaModel.prototype.remove = function(_id, callback) {
+	
+};
+
+module.exports = new FaturaModel();
