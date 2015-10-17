cd09c0e58b8387d2b8882774b80fa51837b21288
dariano <darianosoares@hotmail.com>
2015-10-17 01:24:44 -0300

    Para servir arquivos statics.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(methodOverride('X-Method-Override'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(cors());
