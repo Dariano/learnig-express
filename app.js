b6d9a674075fc95627833965b9a7a47f695ee880
dariano <darianosoares@hotmail.com>
2015-10-16 23:54:43 -0300

    Testando o tipo de retorno, se texto ou json.

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var app = express();
 
 
 app.get('/', function(req, res){
-	res.send('Hello world!')
+	res.status(201);
+	if(req.accepts('text')){
+		res.write('nome; email \n');
+		res.write('Dariano Soares; darianosoares@hotmail.com \n');
+		res.end();
+	} else{
+		res.json({ 'nome': 'Dariano Soares', 'email': 'darianosoares@hotmail.com'})
+	}	
 });
 
 var server = app.listen(3000, function(){
