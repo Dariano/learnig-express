e3135ce563fa52185443b6c883661c410c510780
dariano <darianosoares@hotmail.com>
2015-10-17 14:06:43 -0300

    Resolvido o problema de não encontrar os functions do controller.

--- controllers/faturaController.js
+++ controllers/faturaController.js
@@ -1,23 +1,24 @@
-function FaturaController() {};
-
-FaturaController.prototype.getAll = function(request, response, next){
-	response.send('retorna todas as faturas.');
+function FaturaController() {
+	
+	this.getAll = function(request, response, next){
+		response.send('retorna todas as faturas.');
+	};
+	
+	this.getById= function(request, response, next){
+		response.send('retorna uma fatura.');
+	};
+	
+	this.create = function(request, response, next){
+		response.send('cria uma fatura.');
+	};
+	
+	this.update = function(request, response, next){
+		response.send('atualiza uma fatura.');
+	};
+	
+	this.remove = function(request, response, next){
+		response.send('remove uma fatura.');
+	};
 };
 
-FaturaController.prototype.getById= function(request, response, next){
-	response.send('retorna uma fatura.');
-};
-
-FaturaController.prototype.create = function(request, response, next){
-	response.send('cria uma fatura.');
-};
-
-FaturaController.prototype.update = function(request, response, next){
-	response.send('atualiza uma fatura.');
-};
-
-FaturaController.prototype.remove = function(request, response, next){
-	response.send('remove uma fatura.');
-};
-
-module.exports = FaturaController();
+module.exports = new FaturaController();
