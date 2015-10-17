464b710758e83e0abe8d19740d42f6397528000f
dariano <darianosoares@hotmail.com>
2015-10-17 02:06:14 -0300

    Criando rotas de CRUD da fatura.

--- routes/faturas.js
+++ routes/faturas.js
@@ -5,4 +5,20 @@ router.get('/', function(req, res){
 	res.send('retorna todas as faturas.')
 });
 
+router.get('/:id', function(req, res){
+	res.send('retorna uma fatura.')
+});
+
+router.post('/', function(req, res){
+	res.send('Cria uma fatura.')
+});
+
+router.put('/:id', function(req, res){
+	res.send('atualiza a fatura.')
+});
+
+router.delete('/:id', function(req, res){
+	res.send('remove, ou atualiza a fatura.')
+});
+
 module.exports = router;
